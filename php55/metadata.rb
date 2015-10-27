2d70cced27771e63c0b766ca9745018e6e60151c
Keith Nitsch <keithnitsch@gmail.com>
2015-10-27 11:45:17 -0400

    added php55 upgrade recipe

---
+++ php55/metadata.rb
@@ -0,0 +1,13 @@
+name              "php55"
+maintainer        "Adar Porat"
+maintainer_email "adar.porat@gmail.com"
+license           "Apache 2.0"
+description      "Installs and php 5.5"
+version          "0.0.2"
+
+depends "build-essential"
+depends "apt"
+depends "yum"
+depends "php"
+
+recipe "php55", "Installs php"
