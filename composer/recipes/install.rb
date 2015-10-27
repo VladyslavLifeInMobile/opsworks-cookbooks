4c844786986518a2f0362fbafd74119ada752dd2
Keith Nitsch <keithnitsch@gmail.com>
2015-10-27 13:45:12 -0400

    composer update

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -11,26 +11,4 @@ node[:deploy].each do |app_name, deploy|
     EOH
   end
 
-  template "#{deploy[:deploy_to]}/current/db-connect.php" do
-    source "db-connect.php.erb"
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     (deploy[:database][:host] rescue nil),
-      :user =>     (deploy[:database][:username] rescue nil),
-      :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil)
-    )
-
-   only_if do
-     File.directory?("#{deploy[:deploy_to]}/current")
-   end
-  end
 end
