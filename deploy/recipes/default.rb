239296702e9e1bd4eaac2939d8c867c1e9e6dcb9
Tater <rolnik@lifeinmobile.com>
2016-01-15 14:07:17 +0100

    change port

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'dependencies'
-
+server "gitlab.biuro.3a.pl", user: "deploy", roles: %w{web app db}, port: 10222
 node[:deploy].each do |application, deploy|
-  port = 10222
+  
   opsworks_deploy_user do
     deploy_data deploy
   end
