853fef42ac8cacf8ca2c445972054b3bc0adb8d0
Tater <rolnik@lifeinmobile.com>
2016-01-18 11:42:58 +0100

    add 3a config

--- 3a/recipes/afterdeploy.rb
+++ 3a/recipes/afterdeploy.rb
@@ -1,3 +1,7 @@
+execute 'composer_isntall' do
+    command 'composer install'
+end
+
 
 execute 'assets_install' do
     command '/srv/www/mobilegamifylive/current/app/console assets:install --symlink'
