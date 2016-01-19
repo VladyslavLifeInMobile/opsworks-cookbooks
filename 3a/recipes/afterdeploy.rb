88ea0a1215dee5946ed26369e588b168e65ee309
Tater <rolnik@lifeinmobile.com>
2016-01-19 12:19:54 +0100

    add 3a config

--- 3a/recipes/afterdeploy.rb
+++ 3a/recipes/afterdeploy.rb
@@ -1,11 +1,11 @@
 
 
 execute 'assets_install' do
-    cd /srv/www/mobilegamifylive/current/
+    cd '/srv/www/mobilegamifylive/current/'
     command 'app/console assets:install --symlink'
 end
 
 execute 'assetic_dump' do
-    cd /srv/www/mobilegamifylive/current/
+    cd '/srv/www/mobilegamifylive/current/'
     command 'app/console assetic:dump'
 end
