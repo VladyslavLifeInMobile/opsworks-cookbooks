a5bce3caaf8c45cffaef490cd348715d75860b5a
Tater <rolnik@lifeinmobile.com>
2016-01-19 12:23:33 +0100

    add 3a config

--- 3a/recipes/3asetup.rb
+++ 3a/recipes/3asetup.rb
@@ -1,8 +1,8 @@
 execute 'doctrine_schema' do
-    cd '/srv/www/mobilegamifylive/current/'
+    cwd '/srv/www/mobilegamifylive/current/'
     command 'app/console doctrine:schema:create'
 end
 execute 'khepin' do
-    cd '/srv/www/mobilegamifylive/current/'
+    cwd '/srv/www/mobilegamifylive/current/'
     command 'app/console khepin:yamlfixtures:load'
 end
--- 3a/recipes/afterdeploy.rb
+++ 3a/recipes/afterdeploy.rb
@@ -1,11 +1,11 @@
 
 
 execute 'assets_install' do
-    cd '/srv/www/mobilegamifylive/current/'
+    cwd '/srv/www/mobilegamifylive/current/'
     command 'app/console assets:install --symlink'
 end
 
 execute 'assetic_dump' do
-    cd '/srv/www/mobilegamifylive/current/'
+    cwd '/srv/www/mobilegamifylive/current/'
     command 'app/console assetic:dump'
 end
