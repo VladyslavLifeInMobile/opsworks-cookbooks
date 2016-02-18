4312c82c953d97e6d16e66a0393f88b1b06747ab
Tater <rolnik@lifeinmobile.com>
2016-02-18 12:32:16 +0100

    add source recipe

--- artisan/recipes/migrate.rb
+++ artisan/recipes/migrate.rb
@@ -16,4 +16,27 @@ script "run_migrations" do
 	      php artisan migrate
 	      EOH
 	    end
+
+
+    permissions_command = "sudo chmod -Rf 777 app/storage"
+
+    bash "change_permissions" do
+        Chef::Log.debug("Setting permissions on #{deploy[:deploy_to]}/current/app/storage")
+        cwd "#{deploy[:deploy_to]}/current"
+        code <<-EOH
+            #{permissions_command}
+        EOH
+    end
+
+    permissions_command = "sudo chmod -Rf 777 bootstrap/cache"
+
+    bash "change_permissions" do
+        Chef::Log.debug("Setting permissions on #{deploy[:deploy_to]}/current/boostrap/cache")
+        cwd "#{deploy[:deploy_to]}/current"
+        code <<-EOH
+            #{permissions_command}
+        EOH
+    end
+
+
 end
