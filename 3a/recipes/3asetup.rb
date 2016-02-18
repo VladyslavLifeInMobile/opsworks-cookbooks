e0cfe4c253ee1df827455d33eb1402116a8ec113
Tater <rolnik@lifeinmobile.com>
2016-02-18 10:21:06 +0100

    add source recipe

--- 3a/recipes/3asetup.rb
+++ 3a/recipes/3asetup.rb
@@ -1,8 +1,8 @@
 execute 'doctrine_schema' do
     cwd '/srv/www/mobilegamifylive/current/'
-    command 'app/console doctrine:schema:create --env=prod --no-debug'
+    command 'app/console doctrine:schema:create --env=prod'
 end
 execute 'khepin' do
     cwd '/srv/www/mobilegamifylive/current/'
-    command 'app/console khepin:yamlfixtures:load --env=prod --no-debug'
+    command 'app/console khepin:yamlfixtures:load --env=prod'
 end
--- 3a/recipes/afterdeploy.rb
+++ 3a/recipes/afterdeploy.rb
@@ -2,10 +2,10 @@
 
 execute 'assets_install' do
     cwd '/srv/www/mobilegamifylive/current/'
-    command 'app/console assets:install --symlink --env=prod --no-debug'
+    command 'app/console assets:install --symlink --env=prod'
 end
 
 execute 'assetic_dump' do
     cwd '/srv/www/mobilegamifylive/current/'
-    command 'app/console assetic:dump --env=prod --no-debug'
+    command 'app/console assetic:dump --env=prod'
 end
